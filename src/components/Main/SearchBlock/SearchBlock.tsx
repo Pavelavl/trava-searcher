37be02b00dad66c3255bf1ae67f06f57d5b5ea71
GlavrEd1 <sfdrus2004@gmail.com>
2022-11-09 13:52:54 +0300

    refs

--- src/components/Main/SearchBlock/SearchBlock.tsx
+++ src/components/Main/SearchBlock/SearchBlock.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
 import mic from '../../../assets/mic.svg'
 import micslash from '../../../assets/mic-slash.svg'
@@ -12,10 +12,11 @@ record.lang = 'ru-RU'
 
 const SearchBlock = ({ choice, placeholder }: { choice: string, placeholder: string }): JSX.Element => {
     const navigate = useNavigate()
-    let query: string = document.querySelector('input')?.value!
-    const searchStr = document.querySelector('input')!
+    const searchStr = useRef<HTMLInputElement>(null)
+    let query = searchStr.current!
+    
+    
     const [isListening, setIsListening] = useState(false)
-    const [value, setValue] = useState<string>('')
 
     useEffect(() => {
         handleListen()
@@ -44,8 +45,7 @@ const SearchBlock = ({ choice, placeholder }: { choice: string, placeholder: str
                 .map(result => result.transcript)
                 .join('')
             console.log(transcript)
-            searchStr.value = query = transcript
-            setValue(transcript)
+            query.value = transcript
             record.onerror = (event: any) => {
                 console.log(event.error)
             }
@@ -55,9 +55,7 @@ const SearchBlock = ({ choice, placeholder }: { choice: string, placeholder: str
 
     const handleSubmit = (event: React.FormEvent) => {
         event.preventDefault()
-        query = searchStr.value
-
-        navigate(`/search?q=${query}`)
+        navigate(`/search?q=${query.value}`)
         window.location.reload()
     }
 
@@ -66,7 +64,7 @@ const SearchBlock = ({ choice, placeholder }: { choice: string, placeholder: str
             return (
                 <form onSubmit={handleSubmit}>
                     <div className={styles.searchbox}>
-                        <input className={styles.search} type="text" placeholder={placeholder} />
+                        <input ref={searchStr} className={styles.search} type="text" placeholder={placeholder} />
                         <div onClick={() => setIsListening(prev => !prev)} className={styles.mic}><img src={!isListening ? micslash : mic} alt="" /></div>
                     </div>
                 </form>
@@ -75,7 +73,7 @@ const SearchBlock = ({ choice, placeholder }: { choice: string, placeholder: str
             return (
                 <form onSubmit={handleSubmit} className={styles.subform}>
                     <div className={styles.searchbox_def}>
-                        <input className={styles.search_def} type="text" placeholder={placeholder} />
+                        <input ref={searchStr} className={styles.search_def} type="text" placeholder={placeholder} />
                         <div onClick={() => setIsListening(prev => !prev)} className={styles.mic}><img src={!isListening ? micslash : mic} alt="" /></div>
                     </div>
                     <button type="submit" className={styles.sub}>Найти</button>
